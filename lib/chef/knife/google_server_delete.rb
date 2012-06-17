757f6a8d0595b449acf4afeda52dbf362b1baac9
Chirag Jog <cheezo@Chirags-MacBook-Pro.local>
2012-06-17 21:50:41 +0530

    Handle Empty lists properly while list servers. Rename the chef config param project to google_project

--- lib/chef/knife/google_server_delete.rb
+++ lib/chef/knife/google_server_delete.rb
@@ -32,16 +32,16 @@ class Chef
         :short => "-p PROJECTNAME",
         :long => "--project_id PROJECTNAME",
         :description => "Your Google Compute Project Name",
-        :proc => Proc.new { |project| Chef::Config[:knife][:project] = project } 
+        :proc => Proc.new { |project| Chef::Config[:knife][:google_project] = project } 
 
       def run 
-        unless Chef::Config[:knife][:project]
+        unless Chef::Config[:knife][:google_project]
           ui.error("Project ID is a compulsory parameter")
           exit 1
         end
 
         $stdout.sync = true
-        project_id = Chef::Config[:knife][:project]
+        project_id = Chef::Config[:knife][:google_project]
         validate_project(project_id) 
 
         @name_args.each do |server| 
