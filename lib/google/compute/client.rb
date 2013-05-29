4021998e2f515f3b88cabd0991970882676f193f
chirag <chirag@clogeny.com>
2013-05-29 08:17:19 +0530

    Minor cleanups. Better formatting

--- lib/google/compute/client.rb
+++ lib/google/compute/client.rb
@@ -80,7 +80,9 @@ module Google
         begin
           api_client.authorization.fetch_access_token!
         rescue Faraday::Error::ConnectionFailed => e
-          raise ConnectionFail, "\"The SSL certificates validation may not configured for this system. Please refer README to configured SSL certificates validation\". #{e.message}" if e.message.include? "SSL_connect returned=1 errno=0 state=SSLv3 read server certificate B: certificate verify failed"
+          raise ConnectionFail,
+            "The SSL certificates validation may not configured for this system. Please refer README to configured SSL certificates validation"\
+              if e.message.include? "SSL_connect returned=1 errno=0 state=SSLv3 read server certificate B: certificate verify failed"
         else
           access_token = api_client.authorization.access_token
           refresh_token = api_client.authorization.refresh_token
